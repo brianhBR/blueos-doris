994ef3c089e635848646a9c808a85dd8c4bc96cf
Patrick José Pereira <patrickelectric@gmail.com>
2026-03-11 11:45:16 -0300

    frontend: HomeScreen: Use mdi icon
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- extension/frontend/src/components/HomeScreen.vue
+++ extension/frontend/src/components/HomeScreen.vue
@@ -13,10 +13,9 @@ import {
   Droplets,
   Loader2
 } from 'lucide-vue-next'
+import { mdiCompassOutline } from '@mdi/js'
 import { useSystemStatus, useBattery, useStorage, useLocation, useSensors } from '../composables/useApi'
 import type { SensorModule } from '../composables/useApi'
-
-const mdiCompassOutline = 'M7,17L10.2,10.2L17,7L13.8,13.8L7,17M12,11.1A0.9,0.9 0 0,0 11.1,12A0.9,0.9 0 0,0 12,12.9A0.9,0.9 0 0,0 12.9,12A0.9,0.9 0 0,0 12,11.1M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z'
 import type { Screen } from '../types'
 
 const props = defineProps<{
